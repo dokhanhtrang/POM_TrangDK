d0ece36838e4d20c275060b2f075eda86858f31f
admin <admin@DESKTOP-F4L6MME>
2019-08-22 12:24:02 +0700

    abstract ver1

--- actions/commons/AbstractPage.java
+++ actions/commons/AbstractPage.java
@@ -1,5 +1,9 @@
 package commons;
 
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -51,6 +55,11 @@ public class AbstractPage {
 		select.selectByVisibleText(value);
 	}
 
+	public void selectItemInCustomDropdown(WebDriver driver, String locator, String value) { // Select other options
+		Select select = new Select(driver.findElement(By.xpath(locator)));
+		select.selectByVisibleText(value);
+	}
+
 	public String getFirstItemSelected(WebDriver driver, String locator) {
 		Select select = new Select(driver.findElement(By.xpath(locator)));
 		return select.getFirstSelectedOption().getText();
@@ -59,6 +68,43 @@ public class AbstractPage {
 	public String getAttributeValue(WebDriver driver, String locator, String attribute) {
 		WebElement element = driver.findElement(By.xpath(locator));
 		return element.getAttribute(attribute);
+	}
+
+	public String getTextElement(WebDriver driver, String locator, String value) {
+		WebElement element = driver.findElement(By.xpath(locator));
+		return element.getText();
+	}
+
+	public int countElementNumber(WebDriver driver, String locator, int value) {
+		List<WebElement> elements = driver.findElements(By.xpath(locator));
+		return value = elements.size();
+	}
+
+	public void checkTheCheckbox(WebDriver driver, String locator) {
+		WebElement element = driver.findElement(By.xpath(locator));
+		element.click();
+		assertTrue(element.isSelected());
+	}
+
+	public void unCheckTheCheckbox(WebDriver driver, String locator) {
+		if (driver.findElement(By.xpath(locator)).isSelected()) {
+			driver.findElement(By.xpath(locator)).click();
+		}
+	}
+
+	public boolean isControlDisplayed(WebDriver driver, String locator) {
+		WebElement element = driver.findElement(By.xpath(locator));
+		return element.isDisplayed();
+	}
+
+	public boolean isControlSelected(WebDriver driver, String locator) {
+		WebElement element = driver.findElement(By.xpath(locator));
+		return element.isSelected();
+	}
+
+	public boolean isControlEnabled(WebDriver driver, String locator) {
+		WebElement element = driver.findElement(By.xpath(locator));
+		return element.isEnabled();
 	}
 
 }
